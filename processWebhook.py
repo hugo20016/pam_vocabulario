b630d5648c590ab408a16739dc619c9ded50a1b5
hugo20016 <hugo20016@gmail.com>
2022-09-24 18:13:30 +0200

    asdfasdf

--- processWebhook.py
+++ processWebhook.py
@@ -12,7 +12,7 @@ def favicon():
 @app.route('/')
 @app.route('/home')
 def home():
-    return "Hello World!"
+    return "Hello World!!"
 
 if __name__ == '__main__':
     app.secret_key = 'super secret key'
